53b5127cc0886302d848a858464cc899a10543c4
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-05-20 16:03:19 +0500

    orifjon register ni  bitirdi

--- js/register.js
+++ js/register.js
@@ -1,107 +1,6 @@
-
-
-// let fullname = document.querySelector(".fullname");
-// let password = document.querySelector(".password");
-// let username = document.querySelector(".username");
-// let email = document.querySelector(".email");
-// let inpbox = document.querySelector(".inpbox");
-// let inputs = inpbox.querySelectorAll("input");
-
-// let eyeslash = document.querySelector("#eyeslash");
-// let eye = document.querySelector("#eye");
-// console.log(eyeslash);
-
-// eyeslash.addEventListener("click", function () {
-//     console.log(eyeslash);
-//     password.type = "text";
-//     eyeslash.style.cssText = `
-//     visibility: hidden;
-//   ` ;
-//     eye.style.cssText = `
-//     visibility: visible;
-//   ` ;
-// })
-
-// eye.addEventListener("click", function () {
-//     console.log(eye);
-//     password.type = "password";
-
-//     eye.style.cssText = `
-//     visibility: hidden;
-//   ` ;
-//     eyeslash.style.cssText = `
-//     visibility: visible;
-//   ` ;
-// })
-
-
-
-// inputs.forEach((inp) => {
-
-//     inp.addEventListener("blur", function () {
-//         if (inp.value.trim() === "") {
-//             inp.style.border = "2px solid red";
-//         } else {
-//             inp.style.border = "2px solid green";
-//         }
-//     });
-
-// });
-
-
-// email.addEventListener("input", function () {
-
-//     let value = email.value.trim();
-
-//     let firstPart = value.split("@")[0];
-//     let domain = value.split("@")[1];
-
-//     let isNumber = firstPart.split("").some(char => !isNaN(char));
-//     let isUpper = firstPart.split("").some(char => char === char.toUpperCase() && isNaN(char));
-
-//     if (
-//         domain === "gmail.com" &&
-//         firstPart.length >= 8 &&
-//         isNumber &&
-//         !isUpper
-//     ) {
-//         email.style.border = "2px solid green";
-//     } else {
-//         email.style.border = "2px solid red";
-//     }
-// });
-
-
-// password.addEventListener("input", function () {
-
-//     let value = password.value.trim();
-
-//     let isNumber = value.split("").some(
-//         char => !isNaN(char) && char !== " "
-//     );
-
-//     let isUpper = value.split("").some(
-//         char => char === char.toUpperCase() &&
-//             char !== char.toLowerCase()
-//     );
-
-//     let isLong = value.length >= 6;
-
-//     if (isNumber && isUpper && isLong) {
-//         password.style.border = "2px solid green";
-//     } else {
-//         password.style.border = "2px solid red";
-//     }
-
-// });
-
-
-
 let eyeslash = document.querySelector("#eyeslash");
 let eye = document.querySelector("#eye");
 let password = document.querySelector(".password");
-
-console.log(eyeslash);
 
 eyeslash.addEventListener("click", function () {
     console.log(eyeslash);
@@ -126,15 +25,11 @@ eye.addEventListener("click", function () {
   ` ;
 })
 
-
-
 const inpbox = document.querySelector(".inpbox")
 let arr = Array.from(inpbox.children)
 arr.forEach((box) => {
     let inp = box.children[1];
     inp.addEventListener("input", function () {
-
- 
 
         let attr = inp.getAttribute("class")
         if (attr === "fullname") {
@@ -182,33 +77,61 @@ arr.forEach((box) => {
 
         }
         else if (attr === "password") {
-            
-    let value = inp.value.trim();
 
-    let isNumber = value.split("").some(
-        char => !isNaN(char) && char !== " "
-    );
+            let value = inp.value.trim();
 
-    let isUpper = value.split("").some(
-        char => char === char.toUpperCase() &&
-            char !== char.toLowerCase()
-    );
+            let isNumber = value.split("").some(
+                char => !isNaN(char) && char !== " "
+            );
 
-    let isLong = value.length >= 6;
+            let isUpper = value.split("").some(
+                char => char === char.toUpperCase() &&
+                    char !== char.toLowerCase()
+            );
 
-    if (isNumber && isUpper && isLong) {
-        inp.style.border = "2px solid #1f883d";
-    } else {
-        inp.style.border = "2px solid #cf222e";
-    }
+            let isLong = value.length >= 6;
+
+            if (isNumber && isUpper && isLong) {
+                inp.style.border = "2px solid #1f883d";
+            } else {
+                inp.style.border = "2px solid #cf222e";
+            }
         }
         else alert("Siz notog'ri Event ishlatyapsiz. 😂");
 
     })
 
     inp.addEventListener("blur", function () {
-    if (!inp.value.trim()) {
-        inp.style.border = "2px solid #cf222e";
+        if (!inp.value.trim()) {
+            inp.style.border = "2px solid #cf222e";
+        }
+    });
+})
+
+const btn = document.querySelector(".sign_btn")
+
+btn.addEventListener("click", function (e) {
+    e.preventDefault()
+    let isError = [];
+    let allInpData = {}
+    arr.forEach((box) => {
+        let inp = box.children[1]
+        let styleAttr = inp.getAttribute("style")
+        isError.push(styleAttr?.includes("rgb(31, 136, 61)"))
+        allInpData[inp.getAttribute("class")] = inp.value
+    })
+    let isTruesy = isError.every((value) => value)
+    if (isTruesy) {
+        fetch("https://692ad7077615a15ff24dd6b2.mockapi.io/api/v1/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(allInpData)
+        })
+            .then((res) => res.status >= 200 && res.status < 300 && (window.location.href = "index.html"))
+            .catch(err => alert(err.message));
+    } else {
+        alert("Error")
     }
-});
 })
