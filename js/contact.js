340d71bc92fa7526f11433bc6fec0c174bca5937
Asadbek <atamirzayevasadbek90@gmail.com>
2026-05-25 15:24:04 +0500

    asadbek cardni bitirdi

--- js/contact.js
+++ js/contact.js
@@ -3,31 +3,9 @@ const modal = document.querySelector(".modal")
 const blur = document.querySelector(".blur")
 const Close = document.querySelector("#close")
 
-function closeAction() {
-    modal.style.cssText = `
-    display: none;
-    `
-    blur.style.cssText = `
-    display: none;
-    `
-}
-
-Close.addEventListener("click", closeAction)
-
-add.addEventListener("click", function () {
-    modal.style.cssText = `
-    display: block;
-    `
-    blur.style.cssText = `
-    display: block;
-    `
-    submit.textContent = "Submit"
-    submit.removeAttribute("edit_id")
-})
-
-
 const submit = document.querySelector("#submit")
 const cardWrapper = document.querySelector(".cardWrapper")
+
 const fullName = document.querySelector(".fUllName")
 const department = document.querySelector(".Department")
 const phoneNumber = document.querySelector(".phoneNumber")
@@ -37,9 +15,77 @@ const address = document.querySelector(".address")
 
 const API = "https://6a0b382921e4456256978f49.mockapi.io/Contact"
 
-function editAndDeleteAction(box, id){
-console.log(box.getAttribute("name"), id);
+
+function closeAction() {
+
+    modal.style.display = "none"
+    blur.style.display = "none"
+
+    fullName.value = ""
+    department.value = ""
+    phoneNumber.value = ""
+    email.value = ""
+    imageInput.value = ""
+    address.value = ""
+
+    submit.textContent = "Submit"
+    submit.removeAttribute("edit_id")
 }
+
+Close.addEventListener("click", closeAction)
+
+
+add.addEventListener("click", function () {
+
+    modal.style.display = "block"
+    blur.style.display = "block"
+
+    submit.textContent = "Submit"
+    submit.removeAttribute("edit_id")
+})
+
+
+function editAndDeleteAction(box, id){
+
+    const action = box.getAttribute("name")
+
+    if(action === "delete"){
+
+        const isDelete = confirm("Haqiqatan ham ochirmoqchimisiz?")
+
+        if(isDelete){
+
+            fetch(`${API}/${id}`, {
+                method: "DELETE"
+            })
+            .then(() => {
+                getContacts()
+            })
+        }
+    }
+
+    else if(action === "edit"){
+
+        fetch(`${API}/${id}`)
+        .then(res => res.json())
+        .then(user => {
+
+            modal.style.display = "block"
+            blur.style.display = "block"
+
+            fullName.value = user.full_name
+            department.value = user.department
+            phoneNumber.value = user.phone
+            email.value = user.email
+            address.value = user.address
+
+            submit.textContent = "Update"
+
+            submit.setAttribute("edit_id", id)
+        })
+    }
+}
+
 
 function createCard(user) {
 
@@ -60,12 +106,41 @@ function createCard(user) {
             </div>  
 
             <div style="display: flex; align-items: center; gap: 7px;">
-                <div name="edit" onClick="editAndDeleteAction(this, ${user.id})" style="width: 20px; height: 20px; background-color: #daf9daff; border-radius: 50%; display: flex; align-items: center; justify-content: center;">
+
+                <div 
+                    name="edit" 
+                    onClick="editAndDeleteAction(this, ${user.id})"
+                    style="
+                        width: 20px;
+                        height: 20px;
+                        background-color: #daf9daff;
+                        border-radius: 50%;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        cursor: pointer;
+                    "
+                >
                     <img src="./assets/icons/edit.svg" alt="">
                 </div>
-                <div name="delete" onClick="editAndDeleteAction(this, ${user.id})" style="width: 20px; height: 20px; background-color: #f4d1d1ff; border-radius: 50%; display: flex; align-items: center; justify-content: center;">
+
+                <div 
+                    name="delete" 
+                    onClick="editAndDeleteAction(this, ${user.id})"
+                    style="
+                        width: 20px;
+                        height: 20px;
+                        background-color: #f4d1d1ff;
+                        border-radius: 50%;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        cursor: pointer;
+                    "
+                >
                     <img src="./assets/icons/trash-gray.svg" alt="">
                 </div>
+
             </div>
         </div>
 
@@ -73,7 +148,9 @@ function createCard(user) {
 
             <p>
                 <img src="./assets/icons/mail-forward.svg" alt="">
-                ${user.email.length > 25 ? user.email.slice(0, 22).padEnd(25, "...") : user.email}
+                ${user.email.length > 25
+                    ? user.email.slice(0, 22).padEnd(25, "...")
+                    : user.email}
             </p>
 
             <p>
@@ -110,12 +187,16 @@ function createCard(user) {
     cardWrapper.appendChild(card)
 }
 
+
+
 function getContacts() {
 
     fetch(API)
         .then(res => res.json())
         .then(data => {
+
             cardWrapper.innerHTML = ""
+
             data.forEach(user => {
                 createCard(user)
             })
@@ -124,18 +205,55 @@ function getContacts() {
 
 getContacts()
 
-
 submit.addEventListener("click", function () {
+
+    const editId = submit.getAttribute("edit_id")
 
     const file = imageInput.files[0]
 
+    if(editId && !file){
+
+        fetch(`${API}/${editId}`)
+        .then(res => res.json())
+        .then(oldUser => {
+
+            const updatedUser = {
+                full_name: fullName.value,
+                department: department.value,
+                phone: phoneNumber.value,
+                email: email.value,
+                image: oldUser.image,
+                address: address.value
+            }
+
+            fetch(`${API}/${editId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(updatedUser)
+            })
+            .then(res => res.json())
+            .then(() => {
+
+                getContacts()
+
+                closeAction()
+            })
+        })
+
+        return
+    }
+
     const reader = new FileReader()
 
-    reader.readAsDataURL(file)
+    if(file){
+        reader.readAsDataURL(file)
+    }
 
     reader.onload = function () {
 
-        const newUser = {
+        const userData = {
             full_name: fullName.value,
             department: department.value,
             phone: phoneNumber.value,
@@ -144,25 +262,45 @@ submit.addEventListener("click", function () {
             address: address.value
         }
 
-        fetch(API, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newUser)
-        })
+        if(editId){
+
+            fetch(`${API}/${editId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userData)
+            })
+            .then(res => res.json())
+            .then(() => {
+
+                getContacts()
+
+                closeAction()
+            })
+        }
+
+        else{
+
+            fetch(API, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userData)
+            })
             .then(res => res.json())
             .then(data => {
 
                 createCard(data)
 
                 closeAction()
-
-                fullName.value = ""
-                department.value = ""
-                phoneNumber.value = ""
-                email.value = ""
-                imageInput.value = ""
             })
+        }
+    }
+
+    if(!file && !editId){
+
+        alert("Iltimos rasm tanlang!")
     }
 })
